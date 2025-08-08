c76d36b9b90f52371de78af25da13882b7134107
PhungVanLong <phungvanlongbn04@gmail.com>
2025-08-08 23:32:50 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+var pattern_1 = @"(0|\+84)(3[2-9]|5[689]|7[06-9]|8[1-9]|9[0-9])[0-9]{7}";
+// var pattern_2= @"(?<!\d)(0|\+84)(3[2-9]|5[689]|7[06-9]|8[1-9]|9[0-9])[0-9]{7}(?!\d)";
+var phoneNumber = File.ReadAllText("fake_vn_phones.txt");
+MatchCollection matches = Regex.Matches(phoneNumber, pattern_1);
+Console.WriteLine($"Found {matches.Count} phone numbers");
+foreach (Match match in matches)
+{
+    Console.WriteLine(match.Value);
+}
