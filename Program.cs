61046a985b73d7786e294a1354c37a7a63d6b63b
PhungVanLong <phungvanlongbn04@gmail.com>
2025-08-09 17:25:16 +0700

    finish

--- Program.cs
+++ Program.cs
@@ -1,11 +1,78 @@
 ﻿using System.Text.RegularExpressions;
+using ClosedXML.Excel;
 
-var pattern_1 = @"(0|\+84)(3[2-9]|5[689]|7[06-9]|8[1-9]|9[0-9])[0-9]{7}";
+
+var phoneNumberPattern = @"\b(0|\+84)(3[2-9]|5[689]|7[06-9]|8[1-9]|9[0-9])[0-9]{7}\b";
 // var pattern_2= @"(?<!\d)(0|\+84)(3[2-9]|5[689]|7[06-9]|8[1-9]|9[0-9])[0-9]{7}(?!\d)";
+var emailPattern = @"\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b";
+var codeIDPattern = @"\b(0[0-9]{2}|1[0-9]{2})\d{9}\b";
+var birthDayPattern = @"\b\d{1,2}[\/\-\.]\d{1,2}[\/\-\.]\d{2,4}\b";
+var namePattern = @"\b([A-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠƯẠ-ỹ][a-zàáâãèéêìíòóôõùúăđĩũơưạ-ỹ]+(?:\s+[A-ZÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠƯẠ-ỹ][a-zàáâãèéêìíòóôõùúăđĩũơưạ-ỹ]+)+)\b";
+var bankPattern = @"\b\d{8,16}\b";
+
+
+
+
+
 var phoneNumber = File.ReadAllText("fake_vn_phones.txt");
-MatchCollection matches = Regex.Matches(phoneNumber, pattern_1);
-Console.WriteLine($"Found {matches.Count} phone numbers");
-foreach (Match match in matches)
+MatchCollection matches = Regex.Matches(phoneNumber, phoneNumberPattern);
+Console.WriteLine($"Found {matches.Count} phone numbers / 30 " );
+
+if(Regex.IsMatch(phoneNumber, phoneNumberPattern))
 {
-    Console.WriteLine(match.Value);
+    foreach (Match match in matches)
+    {
+        Console.WriteLine($"Matched phone number: {match.Value}");
+    }
 }
+else
+{
+    Console.WriteLine("Phone number pattern not matched.");
+}
+
+Console.WriteLine("========================================");
+
+var workbook = new XLWorkbook();
+var filepath = "fake_data_20_rows.xlsx";
+using (var xlWorkbook = new XLWorkbook(filepath))
+{
+    var worksheet = xlWorkbook.Worksheet(1); // Sheet đầu tiên
+    var range = worksheet.RangeUsed(); // Lấy toàn bộ vùng có dữ liệu
+    Console.WriteLine($"Used Range: {worksheet}");
+    var rowcount = range.RowCount();
+    var colcount = range.ColumnCount();
+    Console.WriteLine($"Row count: {rowcount}, Column count: {colcount}");
+    // Mảng chứa các pattern
+    Console.WriteLine("========================================");
+    for (int row = 1; row <= rowcount; row++)
+    {
+        for (int col = 1; col <= colcount; col++)
+        {
+            var cellValue = worksheet.Cell(row, col).GetString();
+
+            if (Regex.IsMatch(cellValue, emailPattern))
+            {
+                Console.WriteLine($" {cellValue}");
+            }
+            if (Regex.IsMatch(cellValue, codeIDPattern))
+            {
+                Console.WriteLine($" {cellValue}");
+            }
+            if (Regex.IsMatch(cellValue, birthDayPattern))
+            {
+                Console.WriteLine($" {cellValue}");
+            }
+            if (Regex.IsMatch(cellValue, namePattern))
+            {
+                Console.WriteLine($" {cellValue}");
+            }
+            if (Regex.IsMatch(cellValue, bankPattern))
+            {
+                Console.WriteLine($" {cellValue}");
+            }
+        }
+        Console.WriteLine("========================================");
+
+    }
+}
+
